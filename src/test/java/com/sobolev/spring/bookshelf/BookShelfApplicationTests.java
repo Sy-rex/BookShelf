15293cd96d77a93e8198fdb1dddc4ba1751076c9
Kirill <sobolevkirill0@gmail.com>
2025-09-19 11:49:28 +0300

    start commit

---
+++ src/test/java/com/sobolev/spring/bookshelf/BookShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobolev.spring.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
